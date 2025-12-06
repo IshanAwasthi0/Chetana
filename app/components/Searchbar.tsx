dc1f3ec3ccf5aeae643dc9d585d4e9ad7fa9806a
Ishan <ishan05awasthi@gmail.com>
2025-12-06 12:29:42 +0545

    added events and community-help and Post component

--- app/components/Searchbar.tsx
+++ app/components/Searchbar.tsx
@@ -1,10 +1,9 @@
 import { Icon } from "@iconify/react";
-import React from 'react'
 
 const Sidebar = () => {
     return (
         <form className="flex flex-row justify-between items-center w-220 h-18 bg-[#FFECC8] border-3 rounded border-black relative">
-          <input type="text" placeholder="Search for Stories, Poems, Fables... " className="text-[#726F63] text-2xl z-50 w-220 h-18 pl-6 pr-6 focus:outline-0 focus:ring-0"/>
+          <input type="text" placeholder="Search for Posts.... " className="text-[#726F63] text-2xl z-50 w-220 h-18 pl-6 pr-6 focus:outline-0 focus:ring-0"/>
           <button className="absolute right-6 cursor-pointer"><Icon icon="majesticons:search-line" width="35" height="35" color="gray"/></button>
         </form>
     )
--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -4,8 +4,8 @@ import { Icon } from '@iconify/react';
 const Sidebar = () => {
     return (
         <>
-        <div className='bg-[#FFECC8] w-84 h-screen '>
-            <div  className='text-[#5C5950] sticky pl-8 space-y-13 pt-30'>
+        <div className='bg-[#FFECC8] w-84 h-screen sticky top-0 left-0'>
+            <div  className='text-[#5C5950] pl-8 space-y-13 pt-30 '>
                 <p className='flex gap-5 text-[24px] items-center'> <Icon icon="mdi:compass" width="36" height="36" /> Explore</p>
                 <p className='flex gap-5 text-[24px] items-center'><Icon icon="fluent:people-community-20-filled" width="36" height="36" /> Community help</p>
                 <p className='flex gap-5 text-[24px] items-center'> <Icon icon="gg:profile" width="36" height="36" />Profile</p>

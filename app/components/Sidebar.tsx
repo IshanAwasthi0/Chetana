262fe9f326a2e17a59c57db99b35ccbb3057b842
siddhartha manandhar <playzsidz@gmail.com>
2025-12-06 13:37:32 +0545

    finished sidebar and fixed searchbar issue

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -4,13 +4,14 @@ import { Icon } from '@iconify/react';
 const Sidebar = () => {
     return (
         <>
-        <div className='bg-[#FFECC8] w-84 h-screen sticky top-0 left-0'>
+        <div className='bg-[#FFECC8] z-50 w-84 h-screen sticky top-0 left-0'>
             <div  className='text-[#5C5950] pl-8 space-y-13 pt-30 '>
-                <p className='flex gap-5 text-[24px] items-center'> <Icon icon="mdi:compass" width="36" height="36" /> Explore</p>
-                <p className='flex gap-5 text-[24px] items-center'><Icon icon="fluent:people-community-20-filled" width="36" height="36" /> Community help</p>
-                <p className='flex gap-5 text-[24px] items-center'> <Icon icon="gg:profile" width="36" height="36" />Profile</p>
-                <p className='flex gap-5 text-[24px] items-center'><Icon icon="ic:sharp-emoji-events" width="36" height="36" />Events</p>
-                <p className='flex gap-5 text-[24px] items-center'><Icon icon="majesticons:logout" width="36" height="36" />Log out</p>
+                <button className='flex gap-5 text-[24px] items-center'> <Icon icon="mdi:compass" width="36" height="36" /> Explore</button>
+                <button className='flex gap-5 text-[24px] items-center'><Icon icon="ic:sharp-create" width="36" height="36" /> Create </button>
+                <button className='flex gap-5 text-[24px] items-center'><Icon icon="ic:sharp-emoji-events" width="36" height="36" />Events</button>
+                <button className='flex gap-5 text-[24px] items-center'><Icon icon="fluent:people-community-20-filled" width="36" height="36" /> Community help</button>
+                <button className='flex gap-5 text-[24px] items-center'> <Icon icon="ix:user-profile-filled" width="36" height="36" />Profile</button>
+                <button className='flex gap-5 text-[24px] items-center'><Icon icon="majesticons:logout-line" width="36" height="36" />Log out</button>
             </div>
         </div>
         </>

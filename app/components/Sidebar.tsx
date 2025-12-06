bac7fb5e9bc706668ff6e5fce19f9febe7cf10cf
siddhartha manandhar <playzsidz@gmail.com>
2025-12-06 11:50:18 +0545

    finished sidebar

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Icon } from '@iconify/react';
 
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <>
+        <div className='bg-[#FFECC8] w-84 h-screen '>
+            <div  className='text-[#5C5950] sticky pl-8 space-y-13 pt-30'>
+                <p className='flex gap-5 text-[24px] items-center'> <Icon icon="mdi:compass" width="36" height="36" /> Explore</p>
+                <p className='flex gap-5 text-[24px] items-center'><Icon icon="fluent:people-community-20-filled" width="36" height="36" /> Community help</p>
+                <p className='flex gap-5 text-[24px] items-center'> <Icon icon="gg:profile" width="36" height="36" />Profile</p>
+                <p className='flex gap-5 text-[24px] items-center'><Icon icon="ic:sharp-emoji-events" width="36" height="36" />Events</p>
+                <p className='flex gap-5 text-[24px] items-center'><Icon icon="majesticons:logout" width="36" height="36" />Log out</p>
+            </div>
+        </div>
+        </>
     )
 }
 
